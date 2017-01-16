22ae28b7551ec0423fbfa14ef40077f0ab9d6f08
desezgin <devrimerensezgin@outlook.com>
2017-01-16 16:31:40 +0200

    Add files via upload

---
+++ LinBilgin 1.0.sh
@@ -0,0 +1,91 @@
+cd Belgeler
+echo              >>rapor.log
+echo Sorular için eposta adresi:devrimerensezgin@rocketmail.com>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:date>>rapor.log
+date>rapor.log
+echo              >>rapor.log
+echo Verilen komut:uptime>>rapor.log
+echo              >>rapor.log
+uptime>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:pwd>>rapor.log
+echo              >>rapor.log
+pwd>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:hostname>>rapor.log
+echo              >>rapor.log
+hostname>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:who>>rapor.log
+echo              >>rapor.log
+who>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:whoami>>rapor.log
+echo              >>rapor.log
+whoami>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:finger root>>rapor.log
+echo              >>rapor.log
+finger root>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:last>>rapor.log
+echo              >>rapor.log
+last>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:ps>>rapor.log
+echo              >>rapor.log
+ps>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:uname -a>>rapor.log
+echo              >>rapor.log
+uname -a>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:free>>rapor.log
+echo              >>rapor.log
+free>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:df -h>>rapor.log
+echo              >>rapor.log
+df -h>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:du -sk>>rapor.log
+echo              >>rapor.log
+du -sk>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:cat /proc/interrupts>>rapor.log
+echo              >>rapor.log
+cat /proc/interrupts >>rapor.log
+echo              >>rapor.log
+echo Verilen komut:cat /proc/version>>rapor.log
+echo              >>rapor.log
+cat /proc/version>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:cat /proc/filesystems>>rapor.log
+echo              >>rapor.log
+cat /proc/filesystems>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:cat /etc/printcap>>rapor.log
+echo              >>rapor.log
+cat /etc/printcap>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:lsmod>>rapor.log
+echo              >>rapor.log
+lsmod>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:set>>rapor.log
+echo              >>rapor.log
+set>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:lspci>>rapor.log
+echo              >>rapor.log
+lspci>>rapor.log
+echo              >>rapor.log
+echo Verilen komut:lsusb>>rapor.log
+echo              >>rapor.log
+lsusb>>rapor.log
+echo              >>rapor.log
+echo              >>rapor.log
+echo Hazırlayan:Devrim Eren SEZGİN.>>rapor.log
+echo LinBilgin 1.0 rapor dökümünü incelemektesiniz.>>rapor.log
+echo Sorular için eposta adresi:devrimerensezgin@rocketmail.com>>rapor.log
